7efe9cd65f0ca7f0c6ed2c76948d86c171a1efd1
Kiril Georgiev <hammer3@abv.bg>
2017-10-20 18:37:47 +0300

    6-3 and bux fixed

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -142,32 +142,32 @@ function startApp() {
             if (adverts.length === 0) {
                 $('#ads').text('No advertisements available.');
             } else {
-					let advertsTable = $('<table>')
-						.append($('<tr>').append(
-							'<th>Title</th>',
-							'<th>Description</th>',
-							'<th>Publisher</th>',
-							'<th>Date Published</th>',
-							'<th>Price</th>',
-							'<th>Actions</th>')
-						);
+				let advertsTable = $('<table>')
+					.append($('<tr>').append(
+						'<th>Title</th>',
+						'<th>Description</th>',
+						'<th>Publisher</th>',
+						'<th>Date Published</th>',
+						'<th>Price</th>',
+						'<th>Actions</th>')
+					);
 
-					for (let advert of adverts) {
-						let readMoreLink = $(`<a data-id="${advert._id}" href="#">[Read More]</a>`)
-							.click(function() { displayAdvert($(this).attr("data-id")) });
+				for (let advert of adverts) {
+					let readMoreLink = $(`<a data-id="${advert._id}" href="#">[Read More]</a>`)
+						.click(function() { displayAdvert($(this).attr("data-id")) });
 
-						advertsTable.append($('<tr>').append(
-							$('<td>').text(advert.title),
-							$('<td>').text(advert.description),
-							$('<td>').text(advert.publisher),
-							$('<td>').text(advert.datePublished),
-							$('<td>').text(advert.price),
-							$('<td>').append(readMoreLink)
-						));
-					}
-                }
-                $('#ads').append(advertsTable);
-            }
+					advertsTable.append($('<tr>').append(
+						$('<td>').text(advert.title),
+						$('<td>').text(advert.description),
+						$('<td>').text(advert.publisher),
+						$('<td>').text(advert.datePublished),
+						$('<td>').text(advert.price),
+						$('<td>').append(readMoreLink)
+					));
+				}
+				
+				$('#ads').append(advertsTable);
+			}
         }
     }
 
@@ -176,34 +176,33 @@ function startApp() {
 			kinveyAppKey + "/adverts/" + advertId;
 		const kinveyAuthHeaders = {
 			'Authorization': "Kinvey " + sessionStorage.getItem('authToken'),
-	};
+		};
 
-	$.ajax({
-		method: "GET",
-		url: kinveyAdvertUrl,
-		headers: kinveyAuthHeaders,
-		success: displayAdvertSuccess
-	});
-
-	$('#viewDetailsAd').empty();
-
-	function displayAdvertSuccess(advert) {
-		let advertInfo = $('<div>').append(
-			$('<img>').attr("src", advert.image),
-			$('<br>'),
-			$('<label>').text('Title:'),
-			$('<h1>').text(advert.title),
-			$('<label>').text('Description:'),
-			$('<p>').text(advert.description),
-			$('<label>').text('Publisher:'),
-			$('<div>').text(advert.publisher),
-			$('<label>').text('Date:'),
-			$('<div>').text(advert.datePublished));
-
-		$('#viewDetailsAd').append(advertInfo);
-
-		showView('viewDetailsAd');
-	}
+		$.ajax({
+			method: "GET",
+			url: kinveyAdvertUrl,
+			headers: kinveyAuthHeaders,
+			success: displayAdvertSuccess
+		});
 	
-
+		$('#viewDetailsAd').empty();
+	
+		function displayAdvertSuccess(advert) {
+			let advertInfo = $('<div>').append(
+				$('<img>').attr("src", advert.image),
+				$('<br>'),
+				$('<label>').text('Title:'),
+				$('<h1>').text(advert.title),
+				$('<label>').text('Description:'),
+				$('<p>').text(advert.description),
+				$('<label>').text('Publisher:'),
+				$('<div>').text(advert.publisher),
+				$('<label>').text('Date:'),
+				$('<div>').text(advert.datePublished));
+	
+			$('#viewDetailsAd').append(advertInfo);
+	
+			showView('viewDetailsAd');
+		}
+	}
 }
